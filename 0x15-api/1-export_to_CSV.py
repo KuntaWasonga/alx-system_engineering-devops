7ff6569ac3516089759c4a882f5e26a19d8ab673
KuntaWasonga <wkunta@rocketmail.com>
2023-01-16 11:49:27 -0800

    Corrected code

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+""" Returns data from a REST API and exports it to CSV"""
 import csv
 import requests
 import sys
 
+
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}.".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    user_url = requests.get(url + "users/{}".format(user_id)).json()
+    username = user_url.get("username")
+    todos_url = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+            [user_id, username, t.get("completed"), t.get("title")])
+            for t in todos_url]
